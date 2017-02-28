7d26d1ee693ca27e3fd65c10b3c463a20923854b
Robert Biehl <rbiehl@irrealdesigns.com>
2017-02-28 15:08:20 +0100

    Support newer React Native 0.40 and greater
    Add conditional import to fix build.
    See "breaking changes" in https://github.com/facebook/react-native/releases/tag/v0.40.0

--- ios/RCTSmooch/RCTSmooch.h
+++ ios/RCTSmooch/RCTSmooch.h
@@ -1,7 +1,12 @@
 // SmoochManager.h
 
+#if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
 #import "RCTUtils.h"
+#else
+#import <React/RCTBridgeModule.h>
+#import <React/RCTUtils.h>
+#endif
 
 @interface SmoochManager : NSObject <RCTBridgeModule>
 @end
